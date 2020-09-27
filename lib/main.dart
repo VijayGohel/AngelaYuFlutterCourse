bf991b0a87f83668636ac89c05e95c6adbc333b7
vijay gohel <gohil5898@gmail.com>
2020-09-27 18:37:11 +0530

    AssetImage

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ void main() {
         ),
         backgroundColor: Colors.deepOrange[400],
         body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://resize.indiatvnews.com/en/resize/newbucket/715_-/2018/02/propose-1517999844.jpg')),
+          child: Image(image: AssetImage('images/heart-icon-by-vexels.png')),
         ),
       ),
     ),
