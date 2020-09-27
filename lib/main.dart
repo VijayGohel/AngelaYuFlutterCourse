8f2b77fb90255f66a0eecca492740b1ccb3811d7
vijay gohel <gohil5898@gmail.com>
2020-09-27 18:07:19 +0530

    working with scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Colors.deepOrange[800],
+        ),
+        backgroundColor: Colors.deepOrange[400],
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://resize.indiatvnews.com/en/resize/newbucket/715_-/2018/02/propose-1517999844.jpg')),
+        ),
       ),
     ),
   );
