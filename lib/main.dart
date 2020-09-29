a3f6cea0d5724e4ee4982127f1ee000d43f48f3a
vijay gohel <gohil5898@gmail.com>
2020-09-29 14:22:27 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Colors.deepOrange[800],
+        ),
+        backgroundColor: Colors.deepOrange[400],
+        body: Center(
+          child: Image(image: AssetImage('images/heart-icon-by-vexels.png')),
+        ),
       ),
     ),
   );
